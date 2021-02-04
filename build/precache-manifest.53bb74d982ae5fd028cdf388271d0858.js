141d0c3d3037c6e64e3c2951d29c4df89bb42b58
Matheus Martins <mmartins.oliveira@outlook.com>
2021-02-04 14:52:36 -0300

    build

---
+++ build/precache-manifest.53bb74d982ae5fd028cdf388271d0858.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67c64a95c652b5a8a2421fc4e822e69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a23a8f57f8cf110fa17",
+    "url": "/static/js/2.09000de7.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.09000de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "476526989014ba38da55",
+    "url": "/static/js/main.a7c38d6e.chunk.js"
+  },
+  {
+    "revision": "366c3952ee795ae06b49",
+    "url": "/static/js/runtime-main.ff812085.js"
+  },
+  {
+    "revision": "93003c4cd44a3c5045ac7b67fb1afc2c",
+    "url": "/static/media/logo.93003c4c.png"
+  }
+]);
